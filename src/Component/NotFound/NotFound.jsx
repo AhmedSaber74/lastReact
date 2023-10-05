1ad01d73559a5e5b855a0069c17c2dc4a50f2155
AhmedSaber <as5113336@gmail.com>
2023-10-05 21:02:14 +0300

    upload

---
+++ src/Component/NotFound/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Error from '../../assets/images/404.png'
+
+export default function NotFound() {
+  return (
+    <div className=' w-100 vh'>{Error}
+    error 404
+    </div>
+  )
+}
